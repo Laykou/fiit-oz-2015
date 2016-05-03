40d3e86fdda4b33ab90a8a2bf7f4168457776f3a
Ladislav Gallay <ladislav.gallay@lentil.sk>
2016-05-03 08:34:22 +0200

    Update separator to process all records

--- separator.rb
+++ separator.rb
@@ -1,37 +1,37 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(adapter: 'postgresql', host: 'laykou.no-ip.org', port: 5432, user: 'postgres', 'password': 'postgres', database: 'oz')
+ActiveRecord::Base.establish_connection(adapter: 'postgresql', host: 'localhost', port: 5432, user: 'postgres', 'password': 'postgres', database: 'oz')
 
-sql = "SELECT id,score, text FROM reviews ORDER BY RANDOM() LIMIT 2000" ##
-records_array = ActiveRecord::Base.connection.execute(sql)
+# sql = "SELECT id,score, text FROM reviews ORDER BY RANDOM() LIMIT 2000" ##
+# records_array = ActiveRecord::Base.connection.execute(sql)
 
 dir_paths = {}
 for i in 0..5
-	dir_paths["#{i}.0"] = "./reviews/#{i}.0"
+	dir_paths["#{i}.0"] = "/media/laykou/Video/oz/reviews/#{i}.0"
 end
 
-records_array.each do |rec| 
-		f = File.new "#{dir_paths[rec["score"]]}/#{rec["id"]}.txt", 'w'
-		f.write(rec["text"])
-		f.close
-end 
+# records_array.each do |rec| 
+#		f = File.new "#{dir_paths[rec["score"]]}/#{rec["id"]}.txt", 'w'
+#		f.write(rec["text"])
+#		f.close
+# end 
 
-# LIMIT = 1000
-# page = 0
+LIMIT = 1000
+page = 0
 
-# while true do
-#     sql = "SELECT id,score, text FROM reviews ORDER BY id LIMIT #{LIMIT} OFFSET #{LIMIT * page}"
-#     records_array = ActiveRecord::Base.connection.execute(sql)
+while true do
+    sql = "SELECT id,score, text FROM reviews ORDER BY id LIMIT #{LIMIT} OFFSET #{LIMIT * page}"
+    records_array = ActiveRecord::Base.connection.execute(sql)
 
-#     if records_array.empty?
-#         break;
-#     end
+    if records_array.num_tuples.zero?
+        break;
+    end
 
-#     records_array.each do |rec| 
-#         f = File.new "#{dir_paths[rec["score"]]}/#{rec["id"]}.txt", 'w'
-#         f.write(rec["text"])
-#         f.close
-#     end
+    records_array.each do |rec| 
+        f = File.new "#{dir_paths[rec["score"]]}/#{rec["id"]}.txt", 'w'
+        f.write(rec["text"])
+        f.close
+    end
 
-#     page += 1
-# end
+    page += 1
+end
